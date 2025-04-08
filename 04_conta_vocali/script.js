192e7e6e4f10d945aa9f368cc08115ae7287c3a4
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-08 15:06:34 +0200

    conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,11 +2,29 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
+const isVowel = (char) => {
+    char = char.toLowerCase();
+    return (
+        char === `a` ||
+        char === `e` ||
+        char === `i` ||
+        char === `o` ||
+        char === `u`
+    )
+}
 
+const countVowels = (word) => {
+    let vowelsNumber = 0;
+    for(i = 0; i < word.length; i++) {
+        const currentChar = word[i];
+        if(isVowel(currentChar)) vowelsNumber += 1;
+    }
+    return vowelsNumber;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(countVowels(word));
 
 
 
