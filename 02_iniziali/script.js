7f2c78ffdd14db7c2a5fcb71f27975c7005dbd5f
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-08 14:34:24 +0200

    iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const namesByinitials = [];
 
 // Dichiara la funzione qui.
+const namesInitials = (names, namesByinitials) => {
+    for(i = 0; i < names.length; i++) {
+        const currentName = names[i];
+        namesByinitials.push(currentName[0]);
+    }
+    return namesByinitials;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(namesInitials(names, namesByinitials));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
